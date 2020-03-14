4f8d986b6d076c26e46b966342f1e35fac3928c9
iceBear67 <icebear67@sfclub.cc>
2020-03-15 02:03:39 +0800

    Optimize some codes.Add CommandFilter

--- src/main/java/cc/sfclub/polar/CommandBase.java
+++ src/main/java/cc/sfclub/polar/CommandBase.java
@@ -4,11 +4,16 @@ import cc.sfclub.polar.events.messages.TextMessage;
 import cc.sfclub.polar.user.User;
 import lombok.Getter;
 
+import java.util.ArrayList;
+
 public abstract class CommandBase {
     @Getter
     public String Perm;
     @Getter
     public String Description;
+    public ArrayList<String> Aliases = new ArrayList<>();
+    @Getter
+    public String Provider = ".*";
 
     public abstract void onCommand(User u, TextMessage Command);
 
