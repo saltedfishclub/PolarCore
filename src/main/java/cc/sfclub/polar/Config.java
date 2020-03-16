f73e099cddad8c2c372a2b5d638ed103e799be2c
iceBear67 <icebear67@sfclub.cc>
2020-03-16 21:12:47 +0800

    clean-up codes

--- src/main/java/cc/sfclub/polar/Config.java
+++ src/main/java/cc/sfclub/polar/Config.java
@@ -5,21 +5,20 @@ import cc.sfclub.polar.user.Group;
 import java.util.ArrayList;
 
 class database {
-    public String host;
-    public String user;
-    public String password;
-    public String database;
+    public String host = "localhost:3306";
+    public String user = "root";
+    public String password = "qaq";
+    public String database = "polar";
 }
 
 public class Config {
     public transient String defaultGroup;
     public int config_version;
-    public String version;
-    public String name;
-    public Boolean debug;
-    public String startsWith;
-    public boolean useWhiteList;
-    public ArrayList<String> WhiteGroups = new ArrayList<>();
+    public String version = "dev";
+    public String name = "Polar";
+    public Boolean debug = false;
+    public String startsWith = "!p";
+    public boolean usePolarSecurity = true;
     public database database = new database();
     public ArrayList<Group> groups = new ArrayList<>();
 }
