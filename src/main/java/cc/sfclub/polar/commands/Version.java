3ebe97af5286d5bf93e86da1d7e6e57ff7d907b8
iceBear67 <icebear67@sfclub.cc>
2020-03-29 23:21:47 +0800

    fix bugs

--- src/main/java/cc/sfclub/polar/commands/Version.java
+++ src/main/java/cc/sfclub/polar/commands/Version.java
@@ -11,7 +11,7 @@ public class Version extends CommandBase {
     @Override
     public void onCommand(User u, TextMessage command) {
         command.reply(new String[]{
-                Core.getConf().name + " v" + Core.getConf().config_version,
+                Core.getConf().name + " " + Core.getConf().version,
                 "PolarCore by iceBear67",
                 "https://github.com/saltedfishclub/PolarCore"
         });
