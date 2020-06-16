0e05535798cff7012d66e6063c0d9213bfc90988
iceBear67 <icebear67@sfclub.cc>
2020-06-17 07:10:36 +0800

    Add catcode,Update version

---
+++ src/main/java/cc/sfclub/catcode/CatCode.java
@@ -0,0 +1,132 @@
+package cc.sfclub.catcode;
+
+import cc.sfclub.catcode.entities.At;
+import cc.sfclub.catcode.entities.Image;
+import org.nutz.repo.Base64;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CatCode {
+    /**
+     * Spilt into array.
+     * For example:
+     * x[x]xx[x]xx ->
+     * x
+     * [x]
+     * xx
+     * [x]
+     * xx
+     *
+     * @param str message contains catCode
+     * @return string array
+     */
+    public static String[] spilt(String str) {
+        char[] ch = str.toCharArray();
+        List<String> list = new LinkedList<>();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < ch.length; i++) {
+            char c = ch[i];
+            area_1:
+            if (c == '[') {
+                if (i > 0 && ch[i - 1] == '\\') break area_1;
+                if (builder.length() > 0) list.add(builder.toString());
+                builder = new StringBuilder();
+                builder.append(c);
+                continue;
+            } else if (c == ']') {
+                if (i > 0 && ch[i - 1] == '\\') break area_1;
+                builder.append(c);
+                list.add(builder.toString());
+                builder = new StringBuilder();
+                continue;
+            }
+            builder.append(c);
+        }
+        if (builder.length() > 0) list.add(builder.toString());
+        String[] strs = new String[list.size()];
+        for (int i = 0; i < strs.length; i++) {
+            strs[i] = list.get(i);
+        }
+        return strs;
+    }
+
+    /**
+     * Parse "at" to a entity.
+     *
+     * @param strAt CatCode
+     * @return At
+     */
+    public static At parseAt(String strAt) {
+        if (!(strAt.startsWith("[") && strAt.endsWith("]"))) {
+            return null;
+        }
+        String str = strAt.replaceFirst("\\[", "");
+        str = str.replaceAll("]", "");
+        if ("AtAll".equals(str)) {
+            At at = new At();
+            at.all = true;
+            return at;
+        }
+        String[] args = str.split(",");
+        At at = new At();
+        long usrId;
+        String[] a;
+        if (args.length == 0) {
+            a = str.split(":");
+        } else {
+            a = args[0].split(":");
+        }
+        if (!isNumeric(a[1])) {
+            return null;
+        }
+        usrId = Long.parseLong(a[1]);
+        at.userId = usrId;
+        return at;
+    }
+
+    /**
+     * Parse Img to Entity
+     *
+     * @param strImg cCode
+     * @return image entity
+     */
+    public static Image parseImg(String strImg) {
+        if (!(strImg.startsWith("[") && strImg.endsWith("]"))) {
+            return null;
+        }
+        String str = strImg.replaceFirst("\\[", "");
+        str = str.replaceAll("]", "");
+        String[] args = str.split(",");
+        Image img = new Image();
+        String[] a;
+        if (args.length == 0) {
+            a = str.split(":");
+        } else {
+            a = args[0].split(":");
+        }
+        if (a[1].endsWith(".jpg") || a[1].endsWith(".png")) {
+            img.ID = a[1];
+            return img;
+        }
+        img.URI = Base64.URLSafe.decode(a[1]);
+        return img;
+    }
+
+    /**
+     * return true if str is number.
+     *
+     * @param str string
+     * @return result
+     */
+    public static boolean isNumeric(String str) {
+        String bigStr;
+        try {
+            bigStr = new BigDecimal(str).toString();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
