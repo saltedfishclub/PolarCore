a0aca43a72c67c8c44facf8bc9dabe2a87093a44
iceBear67 <icebear67@sfclub.cc>
2020-07-11 08:43:29 +0800

    [Fix] Could not get data from Source

--- src/main/java/cc/sfclub/command/Source.java
+++ src/main/java/cc/sfclub/command/Source.java
@@ -2,8 +2,10 @@ package cc.sfclub.command;
 
 import cc.sfclub.events.MessageEvent;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class Source {
     private final MessageEvent messageEvent;
 }
