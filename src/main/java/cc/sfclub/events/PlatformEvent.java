c0ebc0f0ace0d6f40adba288edb8e229a9a1dc74
iceBear67 <icebear67@sfclub.cc>
2020-11-21 11:21:46 +0800

    [Refactor] Events tree.

--- src/main/java/cc/sfclub/events/PlatformEvent.java
+++ src/main/java/cc/sfclub/events/PlatformEvent.java
@@ -1,8 +1,5 @@
 package cc.sfclub.events;
 
-import lombok.Getter;
-
 public abstract class PlatformEvent extends Event {
-    @Getter
-    protected String transform;
+    public abstract String getTransform();
 }
