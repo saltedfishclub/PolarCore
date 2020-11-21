d9f6fa6bf261b58a47abb2ccc38cbce197e4b307
iceBear67 <icebear67@sfclub.cc>
2020-11-21 11:23:00 +0800

    [FIx] MemberRequestJoinEvent

--- src/main/java/cc/sfclub/events/member/MemberRequestJoinEvent.java
+++ src/main/java/cc/sfclub/events/member/MemberRequestJoinEvent.java
@@ -6,4 +6,7 @@ import cc.sfclub.events.MemberEvent;
  * 成员请求加入群聊
  */
 public abstract class MemberRequestJoinEvent extends MemberEvent {
+    public abstract boolean deal(boolean accept);
+
+    public abstract String getMessage();
 }
