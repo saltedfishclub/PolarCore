78f16fe84a9a20cb0e5aa27d8dd3e892209f9dd7
iceBear67 <icebear67@sfclub.cc>
2021-08-05 16:12:55 +0800

    Fix bug on ChatColor Clean up

--- src/main/java/cc/sfclub/polar/loader/Loader.java
+++ src/main/java/cc/sfclub/polar/loader/Loader.java
@@ -1,23 +1,14 @@
 package cc.sfclub.polar.loader;
 
-import asia.kala.ansi.AnsiString;
 import cc.sfclub.polar.Bot;
 import cc.sfclub.polar.EventChannel;
-import cc.sfclub.polar.platfrom.IPlatform;
-import cc.sfclub.polar.platfrom.IPlatformBot;
 import cc.sfclub.polar.platfrom.PlatformManager;
-import cc.sfclub.polar.user.User;
 import cc.sfclub.polar.user.UserManager;
-import cc.sfclub.polar.user.data.UserData;
 import cc.sfclub.polar.util.ChatColor;
 import io.ebean.Database;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.List;
 import java.util.jar.Manifest;
 
 @Slf4j
@@ -26,23 +17,27 @@ public class Loader extends Bot {
         super(userManager, platformManager, database, eventChannel);
     }
     private static Manifest manifest;
-
-    private static final void init(){
+    static {
         try {
-            manifest=new Manifest(Loader.class.getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"));
+            manifest = new Manifest(Loader.class.getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(manifest==null || manifest.getMainAttributes().getValue("Commit-Hash") == null){
+        if (manifest == null || manifest.getMainAttributes().getValue("Commit-Hash") == null) {
             manifest = new Manifest();
-            manifest.getMainAttributes().putValue("Build-Date","Invalid");
-            manifest.getMainAttributes().putValue("Build-By","Invalid");
-            manifest.getMainAttributes().putValue("Commit-Hash","Invalid");
+            manifest.getMainAttributes().putValue("Build-Date", "Invalid");
+            manifest.getMainAttributes().putValue("Build-By", "Invalid");
+            manifest.getMainAttributes().putValue("Commit-Hash", "Invalid");
             manifest.getMainAttributes().putValue("Version","Invalid");
         }
     }
     public static void main(String[] args) {
-        init();
-        log.info(ChatColor.of("&bPolarCore build &a"+manifest.getMainAttributes().getValue("Commit-Hash")+" &bver &a"+manifest.getMainAttributes().getValue("Version")));
+        log.info(ChatColor.of("&bPolarCore initializing..."));
+        String buildVer = manifest.getMainAttributes().getValue("Version");
+        String buildCommit = manifest.getMainAttributes().getValue("Commit-Hash");
+        String buildDate = manifest.getMainAttributes().getValue("Build-Date");
+        String buildBy = manifest.getMainAttributes().getValue("Build-By");
+        boolean thirdparty = !buildBy.equals("runner");
+        log.info(ChatColor.of("&bBuild &a" + buildVer + "-" + buildCommit + " &bDate: &a" + buildDate + " " + (thirdparty ? "&cTHIRD PARTY BUILD BY " + buildBy : "&bCI BUILD")));
     }
 }
