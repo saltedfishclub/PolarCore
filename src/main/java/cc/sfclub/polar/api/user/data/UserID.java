de20f3e0062c631a066db4edbdedb85962a18ec9
iceBear67 <icebear67@sfclub.cc>
2021-11-06 11:36:30 +0800

    fix

--- src/main/java/cc/sfclub/polar/api/user/data/UserID.java
+++ src/main/java/cc/sfclub/polar/api/user/data/UserID.java
@@ -1,21 +1,20 @@
 package cc.sfclub.polar.api.user.data;
 
+import cc.sfclub.polar.api.Bot;
 import lombok.RequiredArgsConstructor;
-
-import java.util.UUID;
 
 /**
  * Utility class that helps you fetch.
  */
 @RequiredArgsConstructor
 public class UserID {
-    private final UUID uuid;
+    private final int id;
 
-    public UUID getUUID() {
-        return uuid;
+    public int getId() {
+        return id;
     }
 
     public UserData getUser() {
-
+        return Bot.getInstance().getUserManager().searchUserDataByID(id);
     }
 }
